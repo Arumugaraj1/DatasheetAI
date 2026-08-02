45496e1ca6be049d6954dea8386d01c372ba1fa0
Arumuga <raj.rgpc9@gmail.com>
2026-08-02 15:30:02 +0530

    Upload DatasheetAI project

--- src/llm.py
+++ src/llm.py
@@ -1,42 +1,15 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+api_key = st.secrets.get("GOOGLE_API_KEY") or os.getenv("GOOGLE_API_KEY")
+
+if not api_key:
+    raise ValueError("GOOGLE_API_KEY not found.")
+
+genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
-
-
-def ask_llm(question, context):
-
-    prompt = f"""
-You are an expert Semiconductor Hardware Engineer.
-
-Answer ONLY using the datasheet context below.
-
-If the answer is not present, reply:
-
-'I could not find this information in the uploaded datasheet.'
-
-Context:
-
-{context}
-
-Question:
-
-{question}
-
-Answer professionally.
-
-Include:
-- Explanation
-- Important Notes
-- Design Precautions
-- Technical Considerations
-"""
-
-    response = model.generate_content(prompt)
-
-    return response.text
